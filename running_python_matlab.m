1e10b0d1f5d7be09b99502d74826f5319537e477
abdullah2891 <abdullah2891@gmail.com>
2015-12-07 12:31:37 +0600

    Snapshot
    Scanning the sky  with single photo-diode(with 2 servos) and
    transmitting with Xbee adapter (Zigbee protocol).
    This project is intended to find the predict the position of light
    sources in sky and predicting the output of solar panel which will allow
    optimization

---
+++ running_python_matlab.m
@@ -0,0 +1,72 @@
+%my directory for serial reading python
+clf
+clear all
+clc
+commandStr='python /users/Abdullah_Rahman/Downloads/solar_dome_demo.py ';
+
+
+
+
+
+
+%call python file when you need data for xbee 
+%the payload information is loaded to 
+%data_python as string object
+tic
+
+ b=0;
+for i=1:1
+    %several snapshots
+    A=[];
+   
+    pos_arm=[];
+for i=1:17
+[status, data_python]= system(commandStr);
+data=str2num(data_python)
+t=10:10:170;
+A=[A data(1:17)];
+
+
+
+
+%%%position of the arm
+if  mod(data(18),10)==0 && data(18)<=170 && data(18)>=10  %filtering out wrong data
+    primary_arm(i)=data(18);
+else
+    primary_arm(i)=data(19);
+end
+%%%%%%%%%%%%%%%%%
+%subplot(2,1,1);
+plot(t,data(1:17),'--o')
+
+
+
+
+
+end
+%x=1:289;
+%plot(x,A,'--o')    
+  
+
+
+
+
+
+matrix=reshape(A,17,17);
+
+%%%%%%%%%%%
+c=[matrix primary_arm(:)];% adding index
+c=sortrows(c,18); %sort the index
+c(:,18) = [];  % delete the row
+matrix=c;
+%%%%%%%%%%%%%
+
+%subplot(2,1,2);
+
+
+drawnow;
+imagesc(matrix,[0 1023])
+colormap(jet(1023))
+end
+
+toc
